f728236b1e871efd9d11362757e5feb791aa8ab4
idbo24 <bolinderida@gmail.com>
2024-11-18 18:10:20 +0100

    background and animation
    i started making an ellipse as a moon background and added a black background behind the ellipse to act as space, the i added the star dots. i started working on everything simultaneously, as well as the start and result screen

--- game.js
+++ game.js
@@ -1,17 +1,129 @@
-function setup() {
+let x = 300;
+let y = 100;
+let velocity = 0;
+let thrust = -1.5;
+let gravity = 0.05;
+let isGameOver = false;
+let isGameWon = false;
 
-  createCanvas(800, 600);
+//start, game, result
+let gameState = "start";
+
+//font setup
+function preload() {
+  //pixelated font
+  font = loadFont;
 }
 
+//star and moon parameters
+let starSpeed = 0.2;
+let stars = [];
 
-let x = 300;
-let y = 300;
-
-function frog(x, y) {
+function setup() {
   createCanvas(600, 800);
   frameRate(30);
 
-  background(255);
+  //star positions
+  for (let i = 0; i < 100; i++) {
+    stars.push({
+      x: random(width),
+      y: random(height),
+      size: random(1, 3),
+      speed: random(0.5, 2),
+    });
+  }
+}
+
+function draw() {
+  if (gameState === "start") {
+    startScreen();
+  } else if (gameState === "game") {
+    gameScreen();
+  } else if (gameState === "result") {
+    resultScreen();
+  }
+}
+
+//start screen
+function startScreen() {
+  background(0);
+  fill(255, 137, 0); //orange red color
+  textAlign(CENTER, CENTER);
+  textFont(font);
+  textSize(32);
+  text("Falling Space Frog", width / 2.3, height / 3);
+  textSize(18);
+  text("Press ENTER to Start", width / 2.3, height / 2);
+
+  //stars for start screen
+  drawStars();
+}
+
+//game screen
+function gameScreen() {
+  background(0);
+  drawStars();
+  moon(300, 200);
+
+  //gravity applied to the frogs velocity (falling effect)
+  velocity += gravity;
+
+  //control the frogs movement with the space key
+  if (keyIsDown(32)) {
+    velocity += thrust;
+  }
+
+  //update the frogs position based on velocity
+  y += velocity;
+
+  //frog drawing
+  frog(x, y);
+
+  //check for collision with the moon surface
+  if (y + 145 >= 500) {
+    if (velocity > 4.9) {
+      //if falling too fast
+      isGameOver = true;
+      isGameWon = false;
+    } else {
+      velocity = 0;
+      y = 500 - 145;
+      isGameOver = true;
+      isGameWon = true;
+    }
+  }
+
+  //if frog falls too far
+  if (y > height) {
+    isGameOver = true;
+    isGameWon = false;
+  }
+
+  //result screen
+  if (isGameOver) {
+    gameState = "result";
+  }
+}
+
+//result screens
+function resultScreen() {
+  background(0);
+  fill(255, 137, 0); //orange red color
+  textAlign(CENTER, CENTER);
+  textFont(font);
+  textSize(32);
+  if (isGameWon) {
+    text("You Won!", width / 2.3, height / 3);
+  } else {
+    fill(255, 0, 0); //red color
+    text("You Lost... Try Again", width / 2.3, height / 3);
+  }
+  textSize(18);
+  text("Press ENTER to Restart", width / 2.3, height / 2);
+}
+
+//frog function
+function frog(x, y) {
   noStroke();
 
   //frog body
@@ -161,10 +273,38 @@ function frog(x, y) {
   pop();
 }
 
-function draw() {
-  background(255, 140, 0);
-  frog(x, y);
-  y = y+1;
-  if (y === 620);
-  
+//moon function
+function moon(x, y) {
+  fill(189, 189, 189);
+  ellipse(x, y + 500, 2000, 700);
+}
+
+//moving stars function
+function drawStars() {
+  fill(255, 255, 255, 150);
+  noStroke();
+  for (let i = 0; i < stars.length; i++) {
+    let s = stars[i];
+    s.x += s.speed * starSpeed;
+    if (s.x > width) {
+      s.x = 0;
+      s.y = random(height);
+    }
+    ellipse(s.x, s.y, s.size, s.size);
+  } 
+}
+
+//key presses
+function keyPressed() {
+  if (keyCode === ENTER) {
+    if (gameState === "start" || gameState === "result") {
+      //reset game
+      x = 300;
+      y = 100;
+      velocity = 0;
+      isGameOver = false;
+      isGameWon = false;
+      gameState = "game";
+    }
+  }
 }
